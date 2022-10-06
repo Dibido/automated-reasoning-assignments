8b31fc20d1d8640975ce4038a52a3fbf4b33a6d5
Dibido <dibran.dokter@ru.nl>
2022-10-06 16:19:03 +0200

    finished assignment 4

--- code/programsafety.py
+++ code/programsafety.py
@@ -1,10 +1,12 @@
 from z3 import *
 
-
 NO_LOOPS = 10
 list = []
 
-for n in range(1,2):
+for n in range(1,11):
+    variables = []
+
+    # retruns a tuple (a,b) where a is the condition fo 700+n and b are the assignments of the variable sof the code. 
     def f(a_prev,b_prev,i,a_prev_name,b_prev_name):
         if (i < NO_LOOPS+1):
             a1_name = a_prev_name + "_aTrue_"+ str(i)
@@ -16,17 +18,23 @@ for n in range(1,2):
             b1 = Int(b1_name)
             a2 = Int(a2_name)
             b2 = Int(b2_name)
-            return Or(And(f(a1,b1,i+1,a1_name,b1_name),a1 == a_prev + 2*b_prev, b1 == b_prev +i ),And(f(a2,b2,i+1,a2_name,b2_name),a2 == a_prev +i,b2 == a_prev+b_prev))
+            
+            f_true = f(a1,b1,i+1,a1_name,b1_name)
+            f_false = f(a2,b2,i+1,a2_name,b2_name)
+
+            return (Or(f_true[0],f_false[0]), And(a1 == a_prev + 2*b_prev, b1 == b_prev +i,a2 == a_prev +i,b2 == a_prev+b_prev,And(f_true[1]),And(f_false[1])))
         else:
-            return b_prev == 700 + n
+            variables.append(b_prev)
+            return (b_prev == 700 + n,True)
 
     a0 = Int("a0")
     b0 = Int("b0")
-    not_crash_c = And(f(a0,b0,1,"a0","b0"),a0 == 1,b0 == 1)
+    not_crash_c = And(f(a0,b0,1,"a0","b0")[0],f(a0,b0,1,"a0","b0")[1],a0 == 1,b0 == 1)
 
     basic_c = [not_crash_c]
 
     # Solve the problem and print the model
+    #c = Context()
     s = Solver()
     s.add(basic_c)
     res = s.check()
@@ -34,13 +42,11 @@ for n in range(1,2):
 
     if res == sat:
         m = s.model()
-        #output = m.evaluate(not_crash_c)
-        #print(output)
-        #for item in m:
-        #    print(item)
-        #print(m)
-        list.append(n)
+        for v in variables:
+            if m.evaluate(v) == 700 + n:
+                print("Crashing path for %d: %s" % ( n , v ))
     else:
-        pass
+        list.append(n)
 
+print("Not crashing n : ")
 print(list)
--- code/programsafetytest.py
+++ code/programsafetytest.py
@@ -1,27 +1,13 @@
 a = 1
 b = 1
-# b0_
-# b1_1_
-# b2_2_
-# b1_3_
-# b1_4_
-# b1_5_
-# b1_6_
-# b2_7_
-# b2_8_
-# b2_9_
-# b1_10 = 710
-list = [False,True,True,True,False,True,False,True,True,False]
- #b0_b1_1_b1_2_b1_3_b2_4_b2_5_b1_6_b2_7_b1_8_b1_9_b1_10 = 701,
- #b0_b1_1_b1_2_b2_3_b1_4_b2_5_b2_6_b1_7_b1_8_b2_9_b2_10 = 701,
- #b0_b1_1_b1_2_b1_3_b2_4_b2_5_b2_6_b1_7_b2_8_b1_9_b1_10 = 701,
-list.reverse()
-#b0_bFalse_1_bTrue_2_bFalse_3_bFalse_4_bTrue_5_bTrue_6_bTrue_7_bTrue_8_bTrue_9_bFalse_10 = 704,
-list = [False, True, False, False, True, True, True, True, True, False]
-print(len(list))
-print(list)
- # b0_bFalse_1_bTrue_2_bTrue_3_bTrue_4_bFalse_5_bTrue_6_bFalse_7_bTrue_8_bTrue_9_bFalse_10 = 701,
-for i in range(1,11):
+
+#b0_bTrue_1_bFalse_2_bTrue_3_bFalse_4_bFalse_5_bFalse_6 = 66,
+#b0_bFalse_1_bTrue_2_bTrue_3_bTrue_4_bTrue_5_bFalse_6 = 66,
+#b0_bFalvse_1_bFalse_2_bTrue_3_bTrue_4_bTrue_5_bTrue_6_bFalse_7_bTrue_8_bFalse_9_bFalse_10 =   701
+list = [True, True, False, True, True, False, True, True, True, False]
+list2 = [False, False, False, True, False, True, False, True, True, False]
+#b0_bFalse_1_bTrue_2_bTrue_3_bFalse_4_bFalse_5_bFalse_6_bFalse_7_bFalse_8_bFalse_9_bFalse_10 = 701,
+for i in range(1, 11):
     if list[i-1]:
         a = a+2*b
         b = b+i
